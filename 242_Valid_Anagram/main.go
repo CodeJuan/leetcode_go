bef9cba5622b00d89592c0dbd0bc59e1bef905e2
codejuan <xh@decbug.com>
2016-03-29 20:34:10 +0800

    new algorithm

--- 242_Valid_Anagram/main.go
+++ 242_Valid_Anagram/main.go
@@ -1,30 +1,44 @@
 package _42_Valid_Anagram
 
-import (
-	"sort"
-	"strings"
-)
+//import (
+//	"sort"
+//	"strings"
+//)
 
 
-func SortString(w string) string {
-	s := strings.Split(w, "")
-	sort.Strings(s)
-	return strings.Join(s, "")
-}
+//func SortString(w string) string {
+//	s := strings.Split(w, "")
+//	sort.Strings(s)
+//	return strings.Join(s, "")
+//}
+//
+//func isAnagram(s string, t string) bool {
+//	len_t := len(t)
+//	len_s := len(s)
+//	if (len_s != len_t){
+//		return false
+//	}
+//
+//	t = SortString(t)
+//	s = SortString(s)
+//
+//	if (s != t){
+//		return  false
+//	}
+//	return true
+//}
 
 func isAnagram(s string, t string) bool {
-	len_t := len(t)
-	len_s := len(s)
-	if (len_s != len_t){
-		return false
+	var counterS [256]int
+	var counterT [256]int
+	for _, c := range s{
+		counterS[c]++;
 	}
-
-	t = SortString(t)
-	s = SortString(s)
-
-	if (s != t){
-		return  false
+	for _, c := range t{
+		counterT[c]++;
 	}
-	return true
+	if counterS == counterT{
+		return true
+	}
+	return false
 }
-
